376c6e0dd2697c02422792ab2b61056032e8abaf
mka <mka@A21PC16>
2025-02-24 20:37:32 +0200

    Add project files.

---
+++ introtoc++/Source.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+void main()
+{
+	cout << "\"...For breakfast I ate my fears...\"\nsong:popayane misce\nauthor:tudi syudi i smert";
+	cout << "Every\n\thunter\n\t\twants\n\t\t\tto\n\t\t\t\tknow\n\t\t\t\t\twhere\n\t\t\t\t\t\tthe\n\t\t\t\t\t\t\tpheasant\n\t\t\t\t\t\t\t\tis.";
+	cout << "Sells:soul\ncondition:good\n...\ntel.:222-22-22\n222     | 222 |\n22\t| 22  |\n22\t| 22  |\nand e.d.";
+}
