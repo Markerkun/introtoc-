5d5d73c30b190e0a8ecaaba9600fc1d8dc1b8d3f
Mark <markmyskovets9@gmail.com>
2025-02-26 14:27:00 +0200

    changing names of .cpp files and adding 01hw.cpp

---
+++ introtoc++/01hw.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+void main()
+{
+    //1
+    int f;
+    int s;
+    int t;
+    cout << "введіть перше число: ";
+    cin >> f;
+    cout << "\nвведіть друге число: ";
+    cin >> s;
+    cout << "\nвведіть третє число: ";
+    cin >> t;
+    cout << "\n" << f << "+" << s << "+" << t << "=" << t + s + f << endl;
+}
